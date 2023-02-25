e93cf96dc3b2a30c8ebfaa86af265997fd21d0d6
utmalik <utmalik@amazon.com>
2023-02-25 11:04:58 +0530

    admin middlwares

--- Middlewares/authJWT.js
+++ Middlewares/authJWT.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const authConfig = require("../configs/auth.config");
+const User = require("../Models/user");
+const { userTypes } = require("../utils/constants");
 
 const verifyToken = (req,res,next)=> {
 
@@ -18,12 +20,39 @@ const verifyToken = (req,res,next)=> {
         }
 
         req.userId = payload.id;
-        
         next();
     })
 
 }
 
+const isAdmin = async (req,res,next)=>{
+
+    const user = await User.findOne({userId:req.userId});
+
+    if(user && user.userTypes === userTypes.admin){
+        next();
+    }else{
+        return res.status(403).send({message:"Only Admin users are allowed to access this route!"})
+    }
+}
+
+const isAdminOrOwnUser = async (req,res,next)=>{
+
+    //this will run only for routes which gives id in params 
+
+    const user = await User.findOne({userId:req.userId});
+
+    if( (user && user.userTypes === userTypes.admin) || (req.params.id===user.id)){
+        next();
+    
+    }
+    else{
+        return res.status(403).send({message:"You users are allowed to access this route!"})
+    }
+}
+
 module.exports = {
-    verifyToken
+    verifyToken,
+    isAdmin,
+    isAdminOrOwnUser
 }
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,11 +1,11 @@
 
 const {createUser, getAllUsers, getUserById} = require("../Controllers/userController");
-const {verifyToken} = require("../Middlewares/authJWT");
+const {verifyToken, isAdmin, isAdminOrOwnUser} = require("../Middlewares/authJWT");
 
 module.exports = function(app){
 
     app.post("/crm/api/v1/users",createUser);
-    app.get("/crm/api/v1/users",[verifyToken],getAllUsers);
-    app.get("/crm/api/v1/users/:id",[verifyToken],getUserById);
+    app.get("/crm/api/v1/users",[verifyToken, isAdmin],getAllUsers);
+    app.get("/crm/api/v1/users/:id",[verifyToken,isAdminOrOwnUser],getUserById);
 
 }
